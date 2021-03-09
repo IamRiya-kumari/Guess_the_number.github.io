cbf33ed2d257e1832336d99d3853095be4bb499b
Riyakumari-ops <riyakumari.rk219@gmail.com>
2021-03-10 00:04:58 +0530

    Create Game.c

---
+++ Game.c
@@ -0,0 +1,25 @@
+// Random Number Guessing Game---->
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int main(){
+    int number,guess,nguess=1;
+    srand(time(0));
+    number = rand()%100 + 1;
+    //printf("The number is %d \n",number);
+    do{
+        printf("Guess the number between 1 to 100\n");
+        scanf("%d",&guess);
+        if(guess>number){
+            printf("Lower number please\n!");
+        }
+        else if(guess<number){
+            printf("Higher number please\n!");
+        }
+        else{
+            printf("You guessed it in %d times\n",nguess);
+        }
+        nguess++;
+    }while(guess!=number);
+    return 0;
+}
